708b9ecb37965b4858679e4af9a9c0d1f49bc38b
João Vitor de Camargo <camargo.dev@gmail.com>
2018-09-10 20:41:21 -0300

    [Part 2] Major fixes. New part-2 folder with new tests.

---
+++ part-2/main.c
@@ -0,0 +1,12 @@
+/*
+Função principal para realização da análise sintática.
+
+Este arquivo será posterioremente substituído, não acrescente nada.
+*/
+#include <stdio.h>
+#include "parser.tab.h" //arquivo gerado com bison -d parser.y
+
+int main (int argc, char **argv)
+{
+    return yyparse();
+}
