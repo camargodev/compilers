b97d1035c1227ab9c8bc9a0ffaf737b4c85451e0
João Vitor de Camargo <camargo.dev@gmail.com>
2018-11-12 08:41:09 -0200

    [Part 5] Fixed tests 2-5

--- part-5/scripts/code_test.sh
+++ part-5/scripts/code_test.sh
@@ -1,6 +1,7 @@
 counter=0
 for file in $(ls tests/code-cases/*.txt); do
 	shortfile="$(basename $file)" 
+	echo "\nFile =" $shortfile
 	./etapa5 < tests/code-cases/$shortfile > tests/code-cases/temp$shortfile
 	diff tests/code-cases/temp$shortfile tests/code-cases/results/$shortfile
 	rm -f tests/code-cases/temp$shortfile
