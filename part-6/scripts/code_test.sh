be0bc981a495864e875b4b76b634b734c74be10e
João Vitor de Camargo <camargo.dev@gmail.com>
2018-11-13 23:32:22 -0200

    [Part 6] Fixed code tests expected results

--- part-6/scripts/code_test.sh
+++ part-6/scripts/code_test.sh
@@ -3,7 +3,7 @@ for file in $(ls tests/code-cases/*.txt); do
 	shortfile="$(basename $file)" 
 	echo $shortfile
 	./etapa6 < tests/code-cases/$shortfile > tests/code-cases/temp$shortfile
-	vdiff=$(diff tests/code-cases/temp$shortfile tests/code-cases/results/$shortfile)
+	diff tests/code-cases/temp$shortfile tests/code-cases/results/$shortfile
 	rm -f tests/code-cases/temp$shortfile
 	counter=$((counter+1))
 done
